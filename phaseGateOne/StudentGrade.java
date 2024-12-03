f8bb8f773ebe3f64c19a11b1ec6a713c3b796ff0
Esther Ikechukwu <estherik18@gmail.com>
2024-12-03 10:51:48 +0100

    1hour

--- phaseGateOne/StudentGrade.java
+++ phaseGateOne/StudentGrade.java
@@ -12,12 +12,51 @@ public class StudentGrade{
 	System.out.print("How many subjects do they offer? ");
 	int numberOfSubjects = input.nextInt();
 
-	scoresOfStudents = {}
+	System.out.print("Saving >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+	System.out.print("Saved Successfully");
+
+}
 
 
+public static int[] printStudentGrade(int[] scoresOfStudent, ){
+	int[][] scoresOfStudents = new int[numberOfStudent, numberOfSubjects]
 
+	for(int index[scoreOfStudents] = 0; index <= numberOfStudent; index++){
+		for(int number = 0; number < numberOfSubject[index]; number++){
+			System.out.print("");
 
+	} 
+
+	ArrayList<Integer> scores = new ArrayList<>();
+
+}
+
+public static String printScoreBoard (String  ){
+	for(int index = 0; index < scoreOfStsudent.length; index++){
+	int total = 0;
+	int average = 0;
+	System.out.print("Student" + (index + 1));
+		for(int number = 0; number < numberOfSubject[index]; number++){
+			System.out.printf("\t%d", score[row][column]);	
+			total += score[row][column];
+		}
+		System.out.printf("\t%d" , total);
+		System.out.println();	
 	}
+	
+
+
+} 
+
+public static String printGradeSummary(){
+
+
+
+
+
+}
+
+	
 
 
 }
