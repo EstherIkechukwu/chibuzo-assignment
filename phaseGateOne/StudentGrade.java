a1d7cee3dc7326b825a340a6a81769d3c3d2230f
Esther Ikechukwu <estherik18@gmail.com>
2024-12-03 10:50:26 +0100

    1hour

---
+++ phaseGateOne/StudentGrade.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Array;
+
+public class StudentGrade{
+	public static void main(String... args){
+	Scanner input = new Scanner(System.in);
+
+	System.out.print("How many students do you have? ");
+	int numberOfStudent = input.nextInt();
+
+	System.out.print("How many subjects do they offer? ");
+	int numberOfSubjects = input.nextInt();
+
+	scoresOfStudents = {}
+
+
+
+
+	}
+
+
+}
